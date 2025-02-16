849107fac3a03c48f8401c22f238925dd6a68dcb
Anatolii Maslov <amaslov78@gmail.com>
2025-02-16 20:16:45 +0300

    ver 0.3.0

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    version = "0.1.0"  # some changes
+    version = "0.3.0"
     print(f"{version = }")
     print("Hello, from ECR-based lambda")
     print(event)
