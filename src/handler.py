8f89f556761f278b217b0426d3fee8ebf5b3b937
Anatolii Maslov <amaslov78@gmail.com>
2025-02-16 20:16:19 +0300

    intermediate commit

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    version = "0.1.0"
+    version = "0.1.0"  # some changes
     print(f"{version = }")
     print("Hello, from ECR-based lambda")
     print(event)
