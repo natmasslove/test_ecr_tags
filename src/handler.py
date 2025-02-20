430367ff963d3d20a01cca89a428a8e6092ad431
Anatolii Maslov <amaslov78@gmail.com>
2025-02-20 12:43:33 +0300

    added helper to test

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,9 @@
+from helpers.first import add
+
+
 def lambda_handler(event, context):
     version = "0.3.0"
     print(f"{version = }")
     print("Hello, from ECR-based lambda")
     print(event)
+    print(f"{add(3,4) = }")
